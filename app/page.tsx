cbdbb5ccf6cb20b829d9f33c506a8f73a1d5da04
angel-iscoding <angel.iscoding@gmail.com>
2024-12-18 22:34:00 -0500

    Agregando Timeline section

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
-import { BackgroundLines } from "@/components/ui/BackgroundLines";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import { BackgroundGradientAnimation } from "@/components/ui/GradientBg";
 import { navItems } from "@/data";
 
 export default function Home() {
